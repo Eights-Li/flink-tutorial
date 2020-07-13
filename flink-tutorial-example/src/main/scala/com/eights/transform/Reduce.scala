b913f2ff9a18e3e390c194c5b740cfae7eeb757d
Eights-Li <yelli.hl@gmail.com>
2020-07-14 00:31:07 +0800

    add rolling agg & reduce demo

---
+++ flink-tutorial-example/src/main/scala/com/eights/transform/Reduce.scala
@@ -0,0 +1,31 @@
+package com.eights.transform
+
+import org.apache.flink.streaming.api.scala._
+
+object Reduce {
+
+  /**
+   * reduce demo
+   *
+   * @param args input args
+   */
+  def main(args: Array[String]): Unit = {
+
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val inputStream: DataStream[(String, List[String])] = env.fromElements(
+      ("en", List("tea")),
+      ("fr", List("vin")),
+      ("en", List("cake")))
+
+    val resDataStream: DataStream[(String, List[String])] = inputStream.keyBy(0)
+      .reduce((a, b) => (a._1, a._2 ::: b._2))
+
+    resDataStream.print()
+
+    env.execute("reduce demo")
+
+  }
+
+}
+
---
+++ flink-tutorial-example/src/main/scala/com/eights/transform/RollingAggregations.scala
@@ -0,0 +1,27 @@
+package com.eights.transform
+
+import org.apache.flink.streaming.api.scala._
+
+object RollingAggregations {
+
+  /**
+   * demo for rolling aggregations
+   *
+   * @param args input args
+   */
+  def main(args: Array[String]): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val inputStream: DataStream[(Int, Int, Int)] = env.fromElements((1, 2, 2),
+      (2, 3, 1),
+      (2, 2, 4),
+      (1, 5, 3))
+
+    val resStream: DataStream[(Int, Int, Int)] = inputStream.keyBy(0).sum(1)
+
+    resStream.print()
+
+    env.execute("rolling aggregations demo")
+  }
+
+}
